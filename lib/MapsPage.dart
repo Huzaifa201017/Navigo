fb3ec08f31c1505c2ccaed000fb60c112a7a420c
Huzaifa <l201017@lhr.nu.edu.pk>
2023-03-12 16:17:16 +0500

    Google Maps Integrated

--- lib/MapsPage.dart
+++ lib/MapsPage.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapsPage extends StatelessWidget {
-  const MapsPage({super.key});
+class MapsPage extends StatefulWidget {
+  const MapsPage({Key? key}) : super(key: key);
 
+  @override
+  State<MapsPage> createState() => MapsPageState();
+}
+class MapsPageState extends State<MapsPage> {
+
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 2,
         title: const Text('Home'),
       ),
 
-      body: const Center(
-        child: Text('Maps Page'),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        mapType: MapType.hybrid,
+        myLocationButtonEnabled: true,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
       ),
 
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.directions),
-      ),
+
     );
   }
 }
