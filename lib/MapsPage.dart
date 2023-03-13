1c74d7dadad995a52e35a3184b68c8f791434fe0
zain <zainulabideen1060@gmail.com>
2023-03-14 02:40:05 +0500

    13th March's Progress

--- lib/MapsPage.dart
+++ lib/MapsPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:navigo/ChooseDestPage.dart' ;
+import 'package:navigo/ChooseDestPage.dart';
 
 class MapsPage extends StatelessWidget {
   const MapsPage({super.key});
@@ -7,33 +7,127 @@ class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-
-      body: const Center(
-        child: Text('Maps Page'),
-      ),
-
-      floatingActionButton:Column(mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(onPressed:(){
-            Navigator.push(context,
-                MaterialPageRoute(builder:(context) =>ChooseDestPage()));
-          }
-            ,
-            child: Text("zain"),
-          ),
-          SizedBox(height: 40),
-          FloatingActionButton(onPressed:(){
-            Navigator.push(context,
-                MaterialPageRoute(builder:(context) =>ChooseDestPage()));
-          }
-            ,
-            child: Icon(Icons.directions),
-          ),
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: const Center(
+          child: Text('Maps Page'),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  backgroundColor: Colors.transparent,
+                  builder: (context) => Container(
+                      height: MediaQuery.of(context).size.height * 0.90,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: new BorderRadius.only(
+                          topLeft: const Radius.circular(25.0),
+                          topRight: const Radius.circular(25.0),
+                        ),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Your Location",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    suffixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.clear),
+                                    )),
+                              ),
+                              SizedBox(height: 5),
+                              TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Insert Mid points",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    suffixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.accessibility),
+                                    )),
+                              ),
+                              SizedBox(height: 5),
+                              TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Search Destination",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    suffixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.search),
+                                    )),
+                              ),
+                            ],
+                          ))),
+                );
+              },
+              child: Text("zain"),
+            ),
+            SizedBox(height: 40),
+            FloatingActionButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  backgroundColor: Colors.transparent,
+                  builder: (context) => Container(
+                      height: MediaQuery.of(context).size.height * 0.90,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: new BorderRadius.only(
+                          topLeft: const Radius.circular(25.0),
+                          topRight: const Radius.circular(25.0),
+                        ),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Your Location",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    suffixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.clear),
+                                    )),
+                              ),
+                              SizedBox(height: 5),
+                              TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Search Destination",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    suffixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.search),
+                                    )),
+                              ),
+                            ],
+                          ))),
+                );
+              },
+              child: Icon(Icons.directions),
+            ),
+          ],
+        ));
   }
 }
